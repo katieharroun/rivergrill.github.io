a351c5f3c0c92bd49ca165cc073bb216998b4c3b
katieharroun.github.io <mkate788@gmail.com>
2016-04-13 14:53:31 -0700

    hu

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>River Grill</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+</head>
+<body id="home">
+	<div id="about" class="clearfix">
+		<header>
+		<label for="toggle">
+			<div class="line"></div>
+			<div class="line"></div>
+			<div class="line"></div>
+		</label>
+		<input type="checkbox" id="toggle">	
+			<nav>
+				<div class="nav">
+					<a class="navlinks" href="index.html">Home</a>
+					<a class="navlinks" href="#hhmenu">Happy Hour</a>					
+					<a class="navlinks" href="#apps">Menu</a>
+					<a class="navlinks" href="#wine">Wine</a>
+					<a class="navlinks" href="events.html">Events</a>
+					<a class="navlinks" href="gallery.html">Gallery</a>
+					<a class="navlinks" href="/reservations/">Reservations</a>
+				</div>	
+			</nav>
+		</header>
+		<section>	
+			<h5 id="tagline">Experience Casually Elegant dining on Tahoe City's 'other' waterfront</h5>
+		</section>
+	</div>	
+		<div id="info">
+			<p>River Grill sits on the banks of the peaceful Truckee River, just downstream from the infamous "Fanny Bridge" and the Lake Tahoe Dam, the lake's only outlet. River Grill offers year-round riverfront dining, featuring enticing California cuisine from seasonal menus of fresh seafood, steaks, and tasty appetizers.</p>
+			<br>
+			<p>During the summer months, enjoy our spacious two-level patio with live music from local musicians playing every night.  <p id="mobilewords">Fall and winter at the River Grill offer a cozy respite from the day's activities with good cheer, great food, and a warm and inviting atmostphere to gather with family, and friends old and new. Springtime at the River Grill is glorious as the river fills and the trees and flowers bloom.</p></p>
+			<h5>Happy Hour at the River Grill</h5>
+			<p>The River Grill Bar is a popular and lively spot for apres-activity (work or play) and was voted Best Happy Hour in North Tahoe.  Our Happy Hour features nightly drink specials and special pricing on appetizers at the bar.  Happy Hour is every night from 5:00-6:30 in the bar area ONLY. Happy Hour availability can change anytime at the discretion of the River Grill.</p>
+			<h1>Phone</h1>
+			<h3>530.581.2644</h3>
+			<h1>Location</h1>
+			<h3>55 West Lake Blvd. Tahoe City, CA, 96145</h3>
+			<h1>Hours</h1>
+			<h3>Open every night at 5pm</h3>
+			<h3>Happy Hour in the Bar area from 5-6:30pm</h3>
+		</div>	
+	</div>	
+	<hr>
+	<div>
+		<div id="hhmenu">
+			<div id="hhtitle" class="mobilepics">
+				<div class="titles">
+					<h1>Happy Hour</h1>
+					<h2>Everyday 5:00-6:30</h2>
+				</div>
+			</div>
+			<hr>	
+			<ul>
+				<h6>Draft Beer:  $4</h6>
+				<li>Stella Artois</li>
+				<li>Deschutes Brewery Rotating Tap</li>
+				<li>Auburn Alehouse Rotating Tap</li>
+				<li>Brewing Lair Rotating Tap</li>
+			</ul>
+			<ul class="clearfix">
+				<h6>House Wines:  $5</h6>
+				<div>
+					<h5>Red:</h5>
+					<li>Rosenblum Syrah</li>
+					<li>Blackstone Merlot</li>
+					<li>Snapdragon Pinot Noir</li>
+					<li>Lange Twins Cabernet</li>
+					<h5>White:</h5>
+					<li>Bogle Chardonnay</li>
+					<li>Hogue Sauvignon Blac</li>
+					<li>La Fierra Pinot Grigio</li>
+				</div>	
+			</ul>
+			<h6>House Margaritas  $5</h6>
+			<h6>Vodka Lemonade  $4.50</h6>
+		</div>		
+		<hr>
+			<div class="wrap">
+				<div id="hhfood" class="mobilepics">
+					<div class="titles">
+						<h1>Happy Hour Food</h1>
+					</div>	
+				</div>
+		<hr>
+			<div class="foodwrap">
+				<h6>River Grill Angus Burger with caesar salad and shoestring fries  $11</h6>
+				<p>add cheddar, gorgonzola, bacon, avocado, guacamole  $1 each, substitute sweet potato fries  $2</p>
+				<h2>Appetizers listed below are all 50% off during Happy Hour!</h2>
+				<h6>Shrimp Taco</h6>
+				<p>spinach, cheese, shrimp and cabbage rolled in a large flour tortilla and grilled, served with guacamole and pico de gallo (two tacos per order)</p>
+				<p>$16</p>
+				<h6>Smoked Baby Back Ribs</h6>
+				<p>with coleslaw and BBQ sauce</p>
+				<p>$16</p>
+				<h6>Pulled Pork Sliders</h6>
+				<p>two sliders with pineapple and cabbage slaw</p>
+				<p>$12</p>
+				<h6>Ceviche</h6>
+				<p>tilapia with mango pineapple salsa and corn tortilla chips</p>
+				<p>$13</p>
+				<h6>Sweet Potato Fries</h6>
+				<p>with ranch dip</p>
+				<p>$8</p>
+				<h2>Happy Hour also includes 25% off starters, salads and soups on our regular menu - excluding the Ahi Tuna Sashimi</h2>
+			</div>
+		</div>
+		<hr>	
+			<div class="wrap clearfix">
+				<div id="apps" class="mobilepics">
+					<div class="titles">
+						<h1>Starters</h1>
+					</div>
+				</div>	
+		<hr>	
+				<div class="left">
+					<h6>Peppered Seared Ahi Sashimi</h6>
+					<p>jicama citrus relish, spring roll, wasabi and soy</p>
+					<p>$17</p>
+					<h6>Crispy Fried Calamari</h6>
+					<p>with chipotle aioli and cocktail sauce</p>
+					<p>$12</p>
+					<h6>Prawn Dynamite</h6>
+					<p>crispy, sweet, hot, and spicy with cilantro and green onions</p>
+					<p>$13</p>
+					<h6>Tempura French Green Beans</h6>
+					<p>with ponzu dip</p>
+					<p>$9</p>
+				</div>
+				<div class="right">
+					<h6>Macaroni & Cheese</h6>
+					<p>applewood smoked bacon, tomatoes, N.Y. sharp white cheddar</p>
+					<p>$10</p>
+					<h6>River Grill Spring Roll</h6>
+					<p>with edamame beans and ponzu dip</p>
+					<p>$9</p>
+					<h6>Spicy Chicken Enchilada</h6>
+					<p>guacamole, roasted chili crema and Feta cheese</p>
+					<p>$11</p>
+					<h6>Four Mini Crab Cakes</h6>
+					<p>topped with chipotle aioli</p>
+					<p>$13</p>
+				</div>
+			</div>	
+		<hr>
+			<div class="wrap">
+				<div id="saladpic" class="mobilepics">
+					<div class="titles">
+						<h1>Salads and Soup</h1>
+					</div>
+				</div>	
+		<hr>		
+				<div id="salads">
+					<h6>Red Pear, Gorgonzola & Candied Pecan Salad</h6>
+					<p>with baby greens and sherry vinaigrette</p>
+					<p>$11</p>
+					<h6>Caesar Salad</h6>
+					<p>(ask for without croutons for gluten free)</p>
+					<p>$9</p>
+					<h6>Farmers Market Salad</h6>
+					<p>baby greens, marinated sugar plum tomatoes, sherry vinaigrette</p>
+					<p>$9</p>
+					<h6>Add chicken to any salad</h6><p>$8</p>
+					<h6>Today's Soup</h6>
+					<p>bowl  $8,  cup  $5</p>
+				</div>
+			</div>	
+		<hr>
+			<div class="wrap clearfix">
+				<div id="mainspic" class="mobilepics">
+					<div class="titles">
+						<h1>Mains</h1>
+					</div>
+				</div>
+		<hr>	
+				<div class="left">
+					<h6>Braised Beef Short Ribs</h6>
+					<p>boneless ribs, gratin potatoes, steamed broccoli, cabernet demi </p>
+					<p> $31</p>
+					<h6>Grilled Fresh Salmon</h6>
+					<p>baby bok choy, edamame beans, shiitake mushrooms, sesame ginger vinaigrette </p>
+					<p> $27</p>
+					<h6>Green Chard Raviolis</h6>
+					<p>sonoma goat cheese, roasted garlic, portabella mushroom, rustic tomato sauce </p>
+					<p> $19</p>
+					<h6>Beef Tenderloin Filet</h6>
+					<p>whipped potatoes, sauteed spinach, cabernet demi </p>
+					<p> $32</p>
+				</div>	
+				<div class="right">
+					<h6>Roasted Moscovy Duck Breast</h6>
+					<p>gingered whipped sweet potatoes, grilled asparagus, cabernet demi</p>
+					<p>  $29</p>
+					<h6>Stuffed Chicken Breast</h6>
+					<p>stuffed with arugula, cheddar and prosciutto, served with whipped potaoes, grilled asparagus and cabernet demi</p>
+					<p>  $23</p>
+					<h6>Grilled Pork Rack Chop</h6>
+					<p>celery root puree, carmelized apples, sauteed kale, bourbon bbq sauce</p>
+					<p>$26</p>
+					<h6>River Grill Angus Burger</h6>
+					<p>caesar salad & shoestring fries  $16</p>
+					<p>add cheddar or gorgonzola  $1</p>
+					<p>add bacon  $1  add avocado or guacamole  $1</p>
+					<p>add sweet potato fries  $2</p>
+				</div>	
+			</div>	
+		<hr>
+			<div id="dessertpic">
+					<div class="titles">
+						<h1>Desserts</h1>
+					</div>
+				</div>		
+			<div id="dessertmen">
+					<h2>Susie's Brownie</h2>
+					<p>Served warm with dulce de leche ice cream and Valrhona chocolate sauce</p>
+					<h2>Profiteroles</h2>
+					<p>Two puff pastries filled with ice cream, topped with warm chocolate sauce, caramel sauce and candied macadamia nuts</p>
+					<h2>Apple Crisp</h2>
+					<p>Served warm with vanilla ice cream</p>
+					<h2>Cheesecake</h2>
+					<p>New York-Style cheesecake with blueberry sauce</p>
+					<h2>Class V River Float</h2>
+					<p>Traditional favorite with root beer and vanilla vodka</p>
+					<h2>River Float</h2>
+					<p>Same as above without the excitement</p>
+					<h2>Ice Cream</h2>
+					<p>Choice of Vanilla, Dulce de Leche, Coffee or Raspberry Sorbet</p>
+			</div>
+	</div>	
+	<hr>
+	<div id="wine">
+		<div id="winelist" class="clearfix">
+			<h1>Wine List</h1>
+			<div class="clearfix wineleft">
+				<h5>Whites</h5>
+				<div class="wines">
+					<h6>By The Glass</h6>
+					<p>Segura Viudas Sparkling/ Spain/175ml(split)<span>$8</span></p>
+					<p>La Fiera Pinot Grigio/ Veneto/ Italy '14<span>$7</span></p>
+					<p>Sterling Sauvignon Blac/ Napa '13<span>$7</span></p>
+					<p>Bogle Chardonnay/ California '13<span>$7</span></p>
+					<p>Sonoma-Cutrer Chardonnay/ Russian River '12<span>$12</span></p>
+					<p>Frank Family Chardonnay/ Napa Valley '13<span>$15</span></p>
+					<p>Dr. Heyden Riesling/ Kabinett/ Germany '12<span>$9</span></p>
+					<p>Elk Cove Rose'/ Willamette Valley '14<span>$9</span></p>
+				</div>
+				<div class="wines">
+					<h6>Half Bottles 375mL</h6>
+					<p>Gruet Brut/ Albuquerque/ NM<span>$25</span></p>
+					<p>Rombauer Chardonnay/ Carneros '13<span>$33</span></p>
+					<p>Pride Mt. Viognier/ Sonoma '13<span>$31</span></p>
+				</div>
+				<div class="wines">
+					<h6>Sparkling Wines & Champagne</h6>
+					<p>J Cuvee/ Brut/Russian River<span>$40</span></p>
+					<p>Perrier-Jouet/ Grand Brut/France<span>$69</span></p>
+					<p>Veuve Clicquot Ponsardin/ France<span>$86</span></p>
+				</div>
+				<div class="wines">
+					<h6>Sauvignon Blac</h6>
+					<p>Sterling/ Napa Valley '13<span>$26</span></p>
+					<p>Nautilus/ Malborough/ New Zealand '14<span>$33</span></p>
+					<p>Frogs Leap/ Napa Valley '14<span>$37</span></p>
+					<p>Groth/ Napa Valley '13<span>$38</span></p>
+					<p>Sancerre la Crele/ D. Thomas & Fils/ France '14<span>$44</span></p>
+					<p>Cakebread Cellars/ Napa Valley '13<span>$50</span></p>
+				</div>
+				<div class="wines">
+					<h6>Chardonnay</h6>
+					<p>Wente/ Riva Ranch/ Arroyo Seco '13<span>$30</span></p>
+					<p>Starmount/ Carneros '12<span>$33</span></p>
+					<p>Veedercrest/ Unoaked/ Napa '10<span>$35</span></p>
+					<p>Lumen/ Santa Barbara '12<span>$38</span></p>
+					<p>Sonoma-Cutrer/ Russian River '13<span>$40</span></p>
+					<p>Keenan/ Spring Mountain/ Napa '13<span>$48</span></p>
+					<p>Ramey/ Sonoma Coast '11<span>$52</span></p>
+					<p>Frank Family/ Carneros '13<span>$54</span></p>
+					<p>Rombauer/ Carneros '13<span>$60</span></p>
+					<p>DuMol/ Russian River Valley '12<span>$82</span></p>
+				</div>
+				<div class="wines">
+					<h6>Alternative Whites</h6>
+					<p>Pighin Pinot Grigio/ Grave del Friuli/ Italy '13<span>$30</span></p>
+					<p>Cooper Mt. Pinot Gris/ Willamette Valley '14<span>$32</span></p>
+					<p>Elk Cove Rose'/ Willamete Valley '14<span>$33</span></p>
+					<p>Dr. Heyden Riesling/ Kabinett/ Germany '12<span>$32</span></p>
+				</div>
+				<div class="wines" id="corkage">
+					<p>Corkage fee<span>$18</span></p>
+				</div>
+			</div>	
+			<div class="clearfix wineright">
+				<h5>Reds</h5>
+				<div class="wines">
+					<h6>By The Glass</h6>
+					<p>Snapdragon Pinot Noir/ CA '13<span>$7</span></p>
+					<p>Morgan Pinot Noir/ Santa Lucia '13<span>$12</span></p>
+					<p>Ottimino Zinfandel/ Russian River '11<span>$10</span></p>
+					<p>Blackstone Merlot/ CA '13<span>$8</span></p>
+					<p>Fitch Mt. Cabernet/ Alexander Valley '12<span>$9</span></p>
+					<p>Rutherford Ranch Cabernet/ Napa '13<span>$12</span></p>
+					<p>Rosenblum Syrah/ CA '11<span>$8</span></p>
+				</div>
+				<div class="wines">
+					<h6>Half Bottles 375mL</h6>
+					<p>Seghesio Zinfandel/ Sonoma '14<span>$26</span></p>
+					<p>Chehalem Pinot Noir/ Willamette Valley '13<span>$32</span></p>
+					<p>Patz&Hall Pinot Noir/Sonoma Coast '13<span>$38</span></p>
+					<p>Keenan Cabernet/ Spring Mt/ Napa '11<span>$40</span></p>
+				</div>
+				<div class="wines">
+					<h6>Pinot Noir</h6>
+					<p>La Follette/ North Coast '13<span>$36</span></p>
+					<p>Pech Merle/Russian River Valley '12<span>$38</span></p>
+					<p>Benton Lane/ Willamette Valley '12<span>$45</span></p>
+					<p>Truckee River/ Tondre Grapefield '14<span>$63</span></p>
+					<p>Fiddlehead/ Fiddlestix/ Santa Rita '11<span>$68</span></p>
+					<p>Penner-Ash/ Willamette Valley '12<span>$76</span></p>
+					<p>Arcadian/ Sleepy Hollow/ Santa Lucia '07<span>$77</span></p>
+					<p>Twomey/ Russian River '12<span>$84</span></p>
+				</div>
+				<div class="wines">
+					<h6>Merlot</h6>
+					<p>Provenance/ Napa Valley '10<span>$35</span></p>
+					<p>Miner/ Stagecoach Vineyard/ Napa '11<span>$60</span></p>
+					<p>Schafer/ Napa Valley '12<span>$86</span></p>
+				</div>	
+				<div class="wines">
+					<h6>Zinfandel</h6>
+					<p>Ottimino/ Russian River '12<span>$38</span></p>
+					<p>Carol Shelton "Wild Thing"/ Mendocino<span>$39</span></p>
+					<p>Charlyn/ Old Vine/ Lodi '11<span>$46</span></p>
+					<p>Frank Family/ Napa Valley '12<span>$60</span></p>
+					<p>Storybook Mountain/ Napa '12<span>$65</span></p>
+					<p>The Prisoner/ Napa Valley '14<span>$72</span></p>
+				</div>
+				<div class="wines">
+					<h6>Cabernet Sauvignon & Blends</h6>
+					<p>Reckless Love/ Sonoma '12<span>$40</span></p>
+					<p>Leah Jorgensen "Loiregon"/Cab Franc/ OR '13<span>$43</span></p>
+					<p>Justin/ Paso Robles '13<span>$44</span></p>
+					<p>Rutherford Ranch/ Napa Valley '13<span>$46</span></p>
+					<p>Heitz Cellars/ Napa Valley '10<span>$76</span></p>
+					<p>Owen Roe/ Yakima Valley/ Washington '12<span>$80</span></p>
+					<p>Groth/ Oakville/ Napa '12<span>$90</span></p>
+					<p>Cain Concept/ "The Benchland"/ Napa '07<span>$134</span></p>
+					<p>Silver Oak/ Napa Valley '11<span>$152</span></p>
+					<p>Pride Mt. Reserve/ Napa-Sonoma '10<span>$199</span></p>
+				</div>
+				<div class="wines">
+					<h6>Alternative Reds</h6>
+					<p>Torbreck "Woodcutter's Shiraz"/ Australia '12<span>$39</span></p>
+					<p>Zaca Mesa Reserve Syrah/ Santa Ynez Valley '10<span>$68</span></p>
+					<p>Trapiche Malbec/ "Broquel"/ Mendoza Argentina '13<span>$32</span></p>
+					<p>Nevada City Winery Barbera/ Sierra Foothills<span>$27</span></p>
+				</div>
+			</div>	
+		</div>
+	</div>	
+	<hr id="wineline">
+	<div id="drinks">
+		<div id="drinkspic" class="mobilepics">
+			<div class="titles">
+				<h1>Drinks</h1>
+			</div>
+		</div>
+		<hr>
+		<div id="drinklist">
+			<h1>Draft Beer</h1>
+			<p>Stella Artois Lager (Belgium)</p>
+			<p>Deschutes Fresh Squeezed IPA (Bend, OR)</p>
+			<p>Rotating Tap from the Auburn Ale House (Auburn, CA)</p>
+			<p>Rotating Tap from the Brewing Lair (Blairsden, CA)</p>
+			<h1>Bottled Beer</h1>
+			<p>Bud, Bud Light, Coors Light, St. Pauli Girl N/A, Corona, Heineken, Newcastle, Sierra Nevada Pale Ale, Blue Moon, Deschutes Black Butte Porter, Estrella Daura Lager (Gluten Free), Crispin Cider, Guinness</p>
+			<ul>
+			<h6>Signature Martinis</h6>
+				<li><span class="bold">Classic Martini</span> Belvedere Vodka with gorgonzola stuffed olives</li>
+				<li><span class="bold">Cucumber-tini</span> Hanson Organic Cucumber Vodka, mint, fresh lime</li>
+				<li><span class="bold">White Grape Martini</span> Skyy Moscato Vodka, frozen grapes</li>
+				<li><span class="bold">Tequilatini</span> Casamigos Silver, Grand Marnier, Cointreau and fresh 	lime</li>
+				<li><span class="bold">Rye Manhattan</span> Bulleit Rye</li>
+				<li><span class="bold">Ginger Lemondrop</span> Grey Goose Le Citron, Domaine de Canton</li>
+				<li><span class="bold">Elderflower Gimlet</span> Hendrick's Gin, St. Germain, fresh lime</li>
+			</ul>
+			<ul>
+				<h6>Specialty Cocktails</h6>
+				<li><span class="bold">Sonoma Spritzer</span> Sonoma-Cutrer, St. Germain, soda, fresh lemon</li>
+				<li><span class="bold">Cadillac Margarita</span> Casamigos Silver, Cointreau, Grand Marnier</li>
+				<li><span class="bold">Foxy Lady</span> Hendrick's Gin, basil, soda, fresh lemon</li>
+				<li><span class="bold">New-Fashioned</span> new twist on an old favorite, with Templeton Rye</li>
+				<li><span class="bold">Miramar</span> Patron Silver, organic agave, soda, fresh orange</li>
+				<li><span class="bold">Kentucky Buck</span> Makers Mark, bitters, ginger beer, fresh lemon</li>
+				<li><span class="bold">Moscow Sunset</span> Stolichnaya, ginger beer, strawberry, fresh orange</li>
+				<li><span class="bold">Local's Lemonade</span> Ketel One, St. Germain, fresh lemon</li>
+			</ul>
+			<ul>
+				<h6>Hot Drinks</h6>
+				<li><span class="bold">Hot Toddy</span> with Jameson Irish Whiskey</li>
+				<li><span class="bold">Nutty Irishman</span> Coffee, Bailey's and Frangelico</li>
+				<li><span class="bold">Hot Buttered Rum</span> topped with whipped cream</li>
+				<li><span class="bold">Mexican Coffee</span> with Tequila and Kahlua</li>
+			</ul>
+		</div>	
+	</div>	
+	<hr>
+	<footer>
+		<div id="footercover">
+			<div id="footwords">
+				<h3>River Grill</h3>
+				<h4>Reservations: (530)581-2644</h4>
+				<h4>55 West Lake Blvd.</h4>
+				<h4>Tahoe City, CA 96145</h4>
+				<h4>Just west of Fanny Bridge at the "Y" in Tahoe City- Intersection of Hwy 28 and 89</h4>
+				<hr class="foothr">
+				<h4>Looking for some Summer Fun? Checkout <a class="trr" target="_blank" href="https://www.truckeeriverrafting.com/index.php">Truckee River Rafting</a></h4>
+				<h4>Find us on <a class="facebook" target="_blank" href="https://www.facebook.com/rivergrilltahoe?fref=ts"><img class="facebook" src="images/facebook.png"></a></h4>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
